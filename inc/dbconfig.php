b8cdb00f19c189c270293d4d9d5a2f672afbc559
unknown <panthayanjames@gmail.com>
2019-08-16 18:04:00 +0530

    Crud operation files include

--- inc/dbconfig.php
+++ inc/dbconfig.php
@@ -11,14 +11,8 @@ class DB_con
 		$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME) or die('error connecting to server'.mysql_error());
 		//mysql_select_db(DB_NAME, $conn) or die('error connecting to database->'.mysql_error());
     }
-
-       
-    
 }
 
-//db connect error testing
-//$db = new DB_con();
 
-//calling method to test
-//$db->connnect();
+
 ?>
